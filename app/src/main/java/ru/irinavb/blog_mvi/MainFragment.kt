4a5b89de07aa99b620305da842f8acb2c6eae352
Irina <balkarovairina@gmail.com>
2022-06-20 14:52:11 +0200

    add: MainActivity and MainFragment

---
+++ app/src/main/java/ru/irinavb/blog_mvi/MainFragment.kt
@@ -0,0 +1,40 @@
+package ru.irinavb.blog_mvi
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import ru.irinavb.blog_mvi.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.main_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
